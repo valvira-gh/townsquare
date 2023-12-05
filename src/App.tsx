10f5b3798a46848f790009ae69d02406fbbdbfdd
virvaldev <valtteri.virtanen.dev@gmail.com>
2023-12-05 18:19:10 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './styles.css';
+import React from 'react'
+
+export const App: React.FC = () => {
+    // code
+
+    return (
+        <div className='App'>
+            <main>
+               <header>
+                   <h1>Town Square</h1>
+                   <h1>Tycoon</h1>
+               </header>
+            </main>
+        </div>
+    )
+}
+
+export default App;
