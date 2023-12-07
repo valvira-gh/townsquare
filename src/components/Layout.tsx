3821c70e8af13f90b34c53ee7742aefeaf9f4682
virvaldev <valtteri.virtanen.dev@gmail.com>
2023-12-08 01:23:27 +0200

    Loading animation...

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,17 @@
-import React, { useState, useEffect } from "react";
-import "./layout.css";
+import React from "react";
+import LoadingAnimation from "../animations/LoadingAnimation";
+import './layout.css';
 
-interface LayoutTypes {
-  date: Date;
-  loadingText: null | string;
-  setLoadingText: React.Dispatch<string>;
-}
-
-const Layout: React.FC<LayoutTypes> = () => {
-  const [loadingText, setLoadingText] = useState("Loading");
-  const [date, setDate] = useState<number>(0);
-  useEffect(() => {
-    const date = new Date();
-  }, []);
+const Layout: React.FC = () => {
+    // code
 
   return (
-    <div className={"window"}>
-      {/*LOADING*/}
-      <h2 className="loading-text">{loadingText}</h2>
-    </div>
-  );
-};
+    <div className="window">
+        <div>
+            <LoadingAnimation />
+         </div>
+      </div>
+    )
+}
 
 export default Layout;
